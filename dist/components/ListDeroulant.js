b3fc97518ba6c99546d746dfe980f8f8393877f1
YU KENT <tianyu.tina@gmail.com>
2023-07-11 14:38:09 +0200

    add style

--- dist/components/ListDeroulant.js
+++ dist/components/ListDeroulant.js
@@ -6,28 +6,28 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
+require("../../lib/index.css");
 var _jsxRuntime = require("react/jsx-runtime");
 var ListDeroulant = function ListDeroulant(_ref) {
   var _onChange = _ref.onChange,
     datas = _ref.datas,
     optionName = _ref.optionName,
     valueName = _ref.valueName;
-  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-    children: [" ", /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-      children: "Hello Wordl"
-    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("select", {
-      className: "",
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    className: "bloc",
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)("select", {
+      id: "myselect",
       onChange: function onChange(e) {
         return _onChange(e.target.value);
       },
       children: datas.map(function (data, index) {
         return /*#__PURE__*/(0, _jsxRuntime.jsx)("option", {
-          className: "",
-          value: valueName ? data[valueName] : '',
+          className: "option",
+          value: valueName ? data[valueName] : data[optionName],
           children: data[optionName]
         }, index);
       })
-    })]
+    })
   });
 };
 var _default = ListDeroulant;
